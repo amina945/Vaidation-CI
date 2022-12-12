4ba7820319f2eda3b8d3cc23f3d8547e49cbe1c0
Amina ben ammar <amina.benammar@esprit.tn>
2022-12-13 00:01:35 +0100

    update test

--- src/test/java/tn/esprit/spring/VoyageurServiceImplTest.java
+++ src/test/java/tn/esprit/spring/VoyageurServiceImplTest.java
@@ -46,12 +46,22 @@ public class VoyageurServiceImplTest {
     @Test
     public void testAjouterVoyageur() {
 
+        Mockito.when(voyageurService.ajouterVoyageur(ArgumentMatchers.any(Voyageur.class))).thenReturn(this.voyageur);
+        Voyageur savedVoyageur= voyageurService.ajouterVoyageur(this.voyageur);
+
+        assertNotNull(savedVoyageur.getIdVoyageur());
+        assertNotNull( savedVoyageur.getNomVoyageur());
+    //	assertTrue(savedVoyageur.getQteMin()>0);
+
+        voyageurService.supprimerVoyageur(savedVoyageur);
 
     }
 
     @Test
     public void testSupprimerVoyageur() {
-   
+        voyageurService.ajouterVoyageur(this.voyageur);
+        voyageurService.supprimerVoyageur(this.voyageur);
+        assertNull(voyageurService.recupererVoyageParId(this.voyageur.getIdVoyageur()));
     }
 
    
@@ -67,7 +77,10 @@ public class VoyageurServiceImplTest {
 
     @Test
     public void testModifierVoyageur() {
-   
+        this.voyageur.setNomVoyageur("Salim");
+        Mockito.when(voyageurRepository.save(this.voyageur)).thenReturn(this.voyageur);
+        Voyageur voyageur = voyageurService.modifierVoyageur(this.voyageur);
+        Assertions.assertEquals(this.voyageur,voyageur);
 
     }
 }
